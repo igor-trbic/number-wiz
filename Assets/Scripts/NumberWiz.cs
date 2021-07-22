5cd2ec0e1668cc43b1442d5d6809ad0143f057e1
Igor Trbic <trbic.igor@gmail.com>
2021-07-22 15:47:13 +0200

    Added game

---
+++ Assets/Scripts/NumberWiz.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWiz : MonoBehaviour
+{
+        [SerializeField] int max;
+        [SerializeField] int min;
+        [SerializeField] TextMeshProUGUI guessTxt;
+        int guess;
+    void Start() {
+        StartGame();
+    }
+
+    void StartGame() {
+        NextGuess();
+    }
+
+    public void OnPressHigher() {
+        min = guess + 1;
+        NextGuess();
+    }
+    public void OnPressLover() {
+        max = guess - 1;
+        NextGuess();
+    }
+
+    void NextGuess () {
+        guess = Random.Range(min, max + 1);
+        if (guessTxt) {
+            guessTxt.text = guess.ToString();
+        }
+    }
+}
